9ade172b6ab8b920f735f6fab754cbd8b06fa5d7
pranoy shetty <pranoy@pranoy12.onmicrosoft.com>
2017-07-20 10:10:22 +0000

    https://github.com/pranoy95/dotnetproject.git

---
+++ WebRole1/Controllers/music.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace MvcApplicationDemo.Controllers
+{
+    public class MusicStoreController : Controller
+    {
+        // GET: MusicStrore
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string ShowMusic(string MusicTitle)
+        {
+            return "You selected " + MusicTitle + " Music";
+        }
+    }
+}
